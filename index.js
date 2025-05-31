87ac5431fded9401d98ddbb03286bca53bdc0dea
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-31 19:04:36 +0600

    added jwt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+var jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 3000;
 require('dotenv').config()
@@ -37,6 +38,13 @@ async function run() {
         const jobsCollection = client.db('careerCode').collection('jobs');
         const applicationsCollection = client.db('careerCode').collection('applications');
 
+        // JWT related API
+        app.post('/jwt', async (req, res) => {
+            const { email } = req.body;
+            const user = { email }
+            const token = jwt.sign(user, 'secret', { expiresIn: '1h' });
+            res.send({ token })
+        })
 
 
         // job post method
@@ -57,12 +65,28 @@ async function run() {
             res.send(result)
         })
 
+        app.get('/jobs/applications', async (req, res) => {
+            const email = req.query.email;
+            const query = { hr_email: email };
+            const jobs = await jobsCollection.find(query).toArray();
+
+            // should use aggregate to have optimum data fetching
+            for (const job of jobs) {
+                const applicationQuery = { jobId: job._id.toString() }
+                const application_count = await applicationsCollection.countDocuments(applicationQuery);
+                job.application_count = application_count
+            }
+            res.send(jobs)
+        })
+
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const result = await jobsCollection.findOne(query);
             res.send(result)
         })
+
+
 
         // applications api
         app.get('/applications', async (req, res) => {
